96a809b691d94e0bd0e744c332928fbbac17ae1e
FredArgoX <fredy.argo.3@gmail.com>
2025-12-17 12:56:01 -0600

    Update app.js

--- app.js
+++ app.js
@@ -60,6 +60,11 @@ const futureDates = [
         assets: "pHEX-L2"
     },
     {
+        date: new Date("July 31, 2026"),
+        label: "July 31, 2026",
+        assets: "pHEX-L2"
+    },
+    {
         date: new Date("December 31, 2026"),
         label: "December 31, 2026",
         assets: "pHEX-L2 <span class='separator'>/</span> eHEX-L2"
